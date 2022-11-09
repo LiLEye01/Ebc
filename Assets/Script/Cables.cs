1bf88eeae1ca8f7fb2c065d3cf59a54d9eeb3b66
Jorge Trujillo <a20310090@ucslp.net>
2022-11-09 00:55:18 -0600

    inicio del mini juego cortar cables

---
+++ Assets/Script/Cables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Cables : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cable1;
+    [SerializeField]
+    private GameObject cable2;
+    [SerializeField]
+    private GameObject cable3;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
